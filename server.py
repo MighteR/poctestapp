c83779fd349e2413dc274fdbd850ee00d7464ac8
Michel Magne <michel.magne@gmail.com>
2018-02-15 10:36:23 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,23 +4,25 @@ import requests
 import os
 
 app = Flask(__name__)
-port = int(os.environ["PATH"])
+port = int(os.environ["PORT"])
+print(port)
 
 @app.route('/', methods=['POST'])
 def index():
+  print(port)
   data = json.loads(request.get_data())
 
   # FETCH THE CRYPTO NAME
-  crypto_name = data['conversation']['memory']['crypto']['raw']
+  crypto_name = data['conversation']['memory']['crypto']['value']
 
   # FETCH BTC/USD/EUR PRICES
   r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
 
   return jsonify(
     status=200,
-    replies=[{ 
-      'type': 'text', 
-      'content': 'Roger that' 
+    replies=[{
+      'type': 'text',
+      'content': 'The price of %s is :\n%f BTC, \n%f USD, and \n%f EUR.' % (crypto_name, r.json()['BTC'], r.json()['USD'], r.json()['EUR'])
     }]
   )
 
